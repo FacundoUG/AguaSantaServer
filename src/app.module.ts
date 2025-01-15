8e04213c02f1c76f807935052bfbd56fca753a62
Fgasser <facundo.gasser.ft@gmail.com>
2025-01-15 00:32:38 -0300

    escucha el .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que el ConfigModule esté disponible globalmente
+    }),
+  ],
 })
 export class AppModule {}
